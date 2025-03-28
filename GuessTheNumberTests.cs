582ada6196a162d34598eb0d7b83eb5e697993b6
LL1ume <151727639+LL1ume@users.noreply.github.com>
2025-03-28 13:32:48 +0100

    Add files via upload

---
+++ GuessTheNumberTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+
+public class GuessTheNumberTests
+{
+    [Fact]
+    public void Guess_LowerThanSecret_ReturnsHigherHint()
+    {
+        var game = new NumberGuessGame(50);  // Загадане число 50
+        string result = game.Guess(30);
+        Assert.Equal("Загадане число більше.", result);
+    }
+
+    [Fact]
+    public void Guess_HigherThanSecret_ReturnsLowerHint()
+    {
+        var game = new NumberGuessGame(50);
+        string result = game.Guess(70);
+        Assert.Equal("Загадане число менше.", result);
+    }
+
+    [Fact]
+    public void Guess_CorrectNumber_ReturnsWinningMessage()
+    {
+        var game = new NumberGuessGame(50);
+        string result = game.Guess(50);
+        Assert.Contains("Вітаю! Ви вгадали число", result);
+    }
+
+    [Fact]
+    public void GetAttempts_AfterThreeGuesses_ReturnsThree()
+    {
+        var game = new NumberGuessGame(50);
+        game.Guess(30);
+        game.Guess(40);
+        game.Guess(50);
+        Assert.Equal(3, game.GetAttempts());
+    }
+}
